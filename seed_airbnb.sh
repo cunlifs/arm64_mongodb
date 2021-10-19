e0ae0248a7ce98dd117a8c4d1ed3511544505eec
Stu Cunliffe <s_cunliffe@uk.ibm.com>
2021-10-19 16:09:26 +0100

    Update seed_airbnb.sh

--- seed_airbnb.sh
+++ seed_airbnb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#mongoimport --authenticationDatabase admin \
-mongoimport --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD \
+mongoimport --authenticationDatabase admin \
+  --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD \
   --db $MONGO_DB_NAME \
   --collection $MONGO_COLLECTION_NAME \
   --type $MONGO_DB_TYPE \
