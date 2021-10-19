4f078045e2fd030c970e28214fa401fe00c4b3f8
Stu Cunliffe <s_cunliffe@uk.ibm.com>
2021-10-19 08:15:13 +0100

    Update seed_airbnb.sh

--- seed_airbnb.sh
+++ seed_airbnb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mongoimport --authenticationDatabase admin \
-  --username root --password abc123 \
-  --db airbnb \
-  --collection listings \
-  --type csv \
-  --file /tmp/listings.csv \
+  --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD \
+  --db $MONGO_DB_NAME \
+  --collection $MONGO_COLLECTION_NAME \
+  --type $MONGO_DB_TYPE \
+  --file /tmp/chicago_listings.csv \
   --headerline
